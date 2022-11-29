5aef58c33e11d7fe3d48755f0eeeccb53863410f
zelenskii <zelenskiy.ivan@gmail.com>
2022-11-29 16:27:45 +0300

    two_task

--- 2_task.py
+++ 2_task.py
@@ -4,9 +4,9 @@
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 n = int(input())
-some_list = []
-fact = 1
+list = []
+given = 1
 for i in range(1, n + 1):
-    fact = fact * i
-    some_list.append(fact)
-print(some_list)
+    given = given * i
+    list.append(given)
+print(list)
